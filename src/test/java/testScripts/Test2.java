4072e008e835ce0788169ec60b571337e36af517
HP <HP@DESKTOP-2SKVUE2>
2023-09-22 08:48:36 +0530

    DEMO

---
+++ src/test/java/testScripts/Test2.java
@@ -0,0 +1,11 @@
+package testScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void tc2() {
+	Reporter.log("Hello SQl", true);
+}
+}
