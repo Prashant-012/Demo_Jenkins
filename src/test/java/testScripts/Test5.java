a23eccc1f2ccd17b7b152c8a7857f10c5b12905f
HP <HP@DESKTOP-2SKVUE2>
2023-09-22 17:02:29 +0530

    hi

---
+++ src/test/java/testScripts/Test5.java
@@ -0,0 +1,11 @@
+package testScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+@Test
+public void tc5() {
+	Reporter.log("Hello jenkins", true);
+}
+}
---
+++ src/test/java/testScripts/Test6.java
@@ -0,0 +1,11 @@
+package testScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+@Test
+public void tc6() {
+	Reporter.log("Hello Github", true);
+}
+}
